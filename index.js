c75fd207c0270a070572f4e7d06665e5b690da02
vv <460870550@qq.com>
2018-03-27 23:58:07 +0800

    wanc

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function main() {
       console.log('打开浏览器完成');
       await page.goto('https://detail.tmall.com/item.htm?spm=a230r.1.14.1.30d41c31oZkUwF&id=563311188106&cm_id=140105335569ed55e27b&abbucket=20');
       console.log('打开网页完成');
-      const price = 1 || await page.$eval('.tm-promo-price .tm-price', el => el.innerHTML);
+      const price = await page.$eval('.tm-promo-price .tm-price', el => el.innerHTML);
       console.log(`当前的时间是 ${format(new Date(), 'HH:mm:ss')}, 当前价格是${price}`)
       const m = +format(new Date(), 'mm');
-      if (m === 59 || parseInt(price, 10) <= 300) {
+      if (parseInt(price, 10) <= 300) {
         mailer(price);
       }
       await browser.close();
@@ -23,7 +23,7 @@ function main() {
       console.log('出错了', error.message);
       process.exit(1);
     }
-  }, 1000 * 5);
+  }, 1000 * 60 * 30);
 }
 
 main();
