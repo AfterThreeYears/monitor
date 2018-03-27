d6bbd284306bda9099858bbc21780b57735be282
vv <460870550@qq.com>
2018-03-27 23:17:04 +0800

    add log

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const mailer = require('./mailer');
 const puppeteer = require('puppeteer');
+const format = require('date-fns/format');
 
-(async () => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://detail.tmall.com/item.htm?spm=a230r.1.14.1.30d41c31oZkUwF&id=563311188106&cm_id=140105335569ed55e27b&abbucket=20');
-    const price = await page.$eval('.tm-promo-price .tm-price', el => el.innerHTML);
-    if (parseInt(price, 10) <= 300) {
-      mailer(price);
+(() => {
+  setInterval(async () => {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto('https://detail.tmall.com/item.htm?spm=a230r.1.14.1.30d41c31oZkUwF&id=563311188106&cm_id=140105335569ed55e27b&abbucket=20');
+      const price = await page.$eval('.tm-promo-price .tm-price', el => el.innerHTML);
+      console.log(`当前的时间是 ${format(new Date(), 'HH:mm:ss')}, 当前价格是${price}`)
+      const m = +format(new Date(), 'mm');
+      if (m === 59 || parseInt(price, 10) <= 300) {
+        mailer(price);
+      }
+      await browser.close();
+    } catch (error) {
+      process.exit('出错了', error.message);
     }
-    await browser.close();
-  } catch (error) {
-    process.exit('出错了', error.message);
-  }
+  }, 1000 * 5);
 })();
