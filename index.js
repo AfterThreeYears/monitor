ddac594bc380e237d0b145a65c284e42df0bf9ef
vv <460870550@qq.com>
2018-03-27 23:50:00 +0800

    add log

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ function main() {
   setTimeout(async () => {
     try {
       const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox']});
+      console.log('打开浏览器');
       const page = await browser.newPage();
+      console.log('打开浏览器完成');
       await page.goto('https://detail.tmall.com/item.htm?spm=a230r.1.14.1.30d41c31oZkUwF&id=563311188106&cm_id=140105335569ed55e27b&abbucket=20');
+      console.log('打开网页完成');
       const price = 1 || await page.$eval('.tm-promo-price .tm-price', el => el.innerHTML);
       console.log(`当前的时间是 ${format(new Date(), 'HH:mm:ss')}, 当前价格是${price}`)
       const m = +format(new Date(), 'mm');
